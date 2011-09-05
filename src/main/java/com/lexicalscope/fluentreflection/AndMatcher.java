01b40ebba8f06ab825ea9808082fa7bf34f67c13
Tim Wood <lexicalscope@gmail.com>
2011-09-05 21:50:31 +0100

    having some kind of compiler disagreement with cloudbees again!

--- src/main/java/com/lexicalscope/fluentreflection/AndMatcher.java
+++ src/main/java/com/lexicalscope/fluentreflection/AndMatcher.java
@@ -29,43 +29,43 @@ import org.hamcrest.Matcher;
  * @param <T>
  */
 final class AndMatcher<T> extends ReflectionMatcher<T> {
-	private final List<Matcher<T>> matchers;
+    private final List<Matcher<T>> matchers;
 
-	private AndMatcher(final List<Matcher<T>> matchers) {
-		this.matchers = matchers;
-	}
+    private AndMatcher(final List<Matcher<T>> matchers) {
+        this.matchers = matchers;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public boolean matchesSafely(final T item) {
-		for (final Matcher<T> matcher : matchers) {
-			if (!matcher.matches(item)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean matchesSafely(final Object item) {
+        for (final Matcher<T> matcher : matchers) {
+            if (!matcher.matches(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public void describeTo(final Description description) {
-		for (int i = 0; i < matchers.size(); i++) {
-			description.appendDescriptionOf(matchers.get(i));
-			if (i + 1 < matchers.size()) {
-				description.appendText(" and ");
-			}
-		}
-	}
+    @Override
+    public void describeTo(final Description description) {
+        for (int i = 0; i < matchers.size(); i++) {
+            description.appendDescriptionOf(matchers.get(i));
+            if (i + 1 < matchers.size()) {
+                description.appendText(" and ");
+            }
+        }
+    }
 
-	/**
-	 * Creates an and matcher combining all the passed matchers
-	 * 
-	 * @param matchers
-	 *            The matchers to be put in and
-	 * @return A matcher that return true if all of the matchers return true
-	 */
-	public static <T> AndMatcher<T> andOf(final List<Matcher<T>> matchers) {
-		return new AndMatcher<T>(matchers);
-	}
+    /**
+     * Creates an and matcher combining all the passed matchers
+     * 
+     * @param matchers
+     *            The matchers to be put in and
+     * @return A matcher that return true if all of the matchers return true
+     */
+    public static <T> AndMatcher<T> andOf(final List<Matcher<T>> matchers) {
+        return new AndMatcher<T>(matchers);
+    }
 }
