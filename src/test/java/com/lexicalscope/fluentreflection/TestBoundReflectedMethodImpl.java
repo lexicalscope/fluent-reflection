f4b12099a64f98f4f54291991126db86e46c655f
Tim Wood <lexicalscope@gmail.com>
2011-09-13 21:05:28 +0100

    improve test coverage

---
+++ src/test/java/com/lexicalscope/fluentreflection/TestBoundReflectedMethodImpl.java
@@ -0,0 +1,42 @@
+package com.lexicalscope.fluentreflection;
+
+import static com.lexicalscope.fluentreflection.FluentReflection.method;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/*
+ * Copyright 2011 Tim Wood
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+public class TestBoundReflectedMethodImpl {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    static class ClassWithStaticMethod {
+        static void method() {
+        }
+    }
+
+    @Test
+    public void bindingStaticMethodIsNotAllowed() throws Exception {
+        exception.expect(IllegalArgumentException.class);
+
+        new BoundReflectedMethodImpl(
+                method(ClassWithStaticMethod.class.getDeclaredMethods()[0]),
+                new ClassWithStaticMethod());
+    }
+}
