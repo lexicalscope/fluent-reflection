c60072a7b9ff2af93418cc3906a4ed77cded48f7
Tim Wood <lexicalscope@gmail.com>
2012-07-15 16:08:11 +0100

    simplify testing

--- fluent-reflection/src/test/java/com/lexicalscope/fluentreflection/TestMatcherElementHasVoidReturn.java
+++ fluent-reflection/src/test/java/com/lexicalscope/fluentreflection/TestMatcherElementHasVoidReturn.java
@@ -1,50 +1,30 @@
 package com.lexicalscope.fluentreflection;
 
+import static com.lexicalscope.fluentreflection.FluentReflection.method;
 import static com.lexicalscope.fluentreflection.ReflectionMatchers.hasType;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.hamcrest.Matcher;
-import org.jmock.Expectations;
 
-public class TestMatcherElementHasVoidReturn extends AbstractTestReflectionMatcher<FluentMember> {
-    @Override
-    protected ReflectionMatcher<FluentMember> matcher() {
+public class TestMatcherElementHasVoidReturn extends AbstractTestReflectionMatcherNoMocks<FluentMember> {
+    interface Klass {
+        void voidMethod();
+        String stringMethod();
+    }
+
+    @Override protected FluentMethod target() {
+        return method(Klass.class, "voidMethod");
+    }
+
+    @Override protected FluentMember failingTarget() {
+        return method(Klass.class, "stringMethod");
+    }
+
+    @Override protected ReflectionMatcher<FluentMember> matcher() {
         return hasType((Class<?>) null);
     }
 
-    @Override
-    protected Matcher<String> hasDescription() {
+    @Override protected Matcher<String> hasDescription() {
         return equalTo("callable with return type <void>");
-    }
-
-    @Override
-    protected void setupFailingCase() {
-        context.checking(new Expectations() {
-            {
-                oneOf(method).type();
-                will(returnValue(type));
-
-                oneOf(type).classUnderReflection();
-                will(returnValue(Object.class));
-            }
-        });
-    }
-
-    @Override
-    protected void setupMatchingCase() {
-        context.checking(new Expectations() {
-            {
-                oneOf(method).type();
-                will(returnValue(type));
-
-                oneOf(type).classUnderReflection();
-                will(returnValue(void.class));
-            }
-        });
-    }
-
-    @Override
-    protected FluentMember target() {
-        return method;
     }
 }
