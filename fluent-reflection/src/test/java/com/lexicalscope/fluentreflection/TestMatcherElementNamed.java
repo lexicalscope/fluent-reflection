716b05e0858319787f18b53a67618e76326f0c93
Tim Wood <lexicalscope@gmail.com>
2012-07-15 16:09:00 +0100

    simplify testing

--- fluent-reflection/src/test/java/com/lexicalscope/fluentreflection/TestMatcherElementNamed.java
+++ fluent-reflection/src/test/java/com/lexicalscope/fluentreflection/TestMatcherElementNamed.java
@@ -1,29 +1,28 @@
 package com.lexicalscope.fluentreflection;
 
+import static com.lexicalscope.fluentreflection.FluentReflection.method;
 import static com.lexicalscope.fluentreflection.ReflectionMatchers.hasName;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.hamcrest.Matcher;
 
-public class TestMatcherElementNamed extends AbstractTestReflectionMatcher<FluentMember> {
-    @Override
-    protected FluentMethod target() {
-        return method;
+public class TestMatcherElementNamed extends AbstractTestReflectionMatcherNoMocks<FluentMember> {
+    interface Klass {
+        void abc();
+        void def();
+    }
+
+    @Override protected FluentMethod target() {
+        return method(Klass.class, "abc");
+    }
+
+    @Override protected FluentMember failingTarget() {
+        return method(Klass.class, "def");
     }
 
     @Override
     protected ReflectionMatcher<FluentMember> matcher() {
         return hasName("abc");
-    }
-
-    @Override
-    protected void setupMatchingCase() {
-        whenMethodHasName("abc");
-    }
-
-    @Override
-    protected void setupFailingCase() {
-        whenMethodHasName("def");
     }
 
     @Override
